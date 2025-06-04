2ff5676047ac120c3866a63db775384b23815e30
leoqq <leofangxh@gmail.com>
2025-06-04 17:39:18 +0100

    Update

--- src/Breakup.c
+++ src/Breakup.c
@@ -206,9 +206,9 @@ if(old_parcel_cloud->thermal_breakup_flag[p_idx]==4){
             // }
 
             // reload after adding parcels
-            load_user_cloud(&old_parcel_cloud, old_parcel_cloud);
-            CONVERGE_index_t new_cloud_size = CONVERGE_cloud_size(old_parcel_cloud);
-            printf("\nNew cloud size = %i",new_cloud_size);
+            // load_user_cloud(&old_parcel_cloud, old_parcel_cloud);
+            // CONVERGE_index_t new_cloud_size = CONVERGE_cloud_size(old_parcel_cloud);
+            // printf("\nNew cloud size = %i",new_cloud_size);
             if(new_cloud_size <= initial_cloud_size)
             {
                 printf("\nError: New cloud size is not larger than initial cloud size after breakup");
