1fd13f0c2262e1fb8e17b86f064ec5e23572616a
leoqq <leofangxh@gmail.com>
2025-06-09 14:39:09 +0100

    Update

--- src/parcel_prop.c
+++ src/parcel_prop.c
@@ -205,7 +205,8 @@ CONVERGE_UDF(parcel_child,
       CONVERGE_vec3_t parent_position, unit_radial_velocity,displacement;
       CONVERGE_vec3_dup(parcel_cloud.xx[passed_parent_parcel_idx], parent_position);
       CONVERGE_vec3_dup(parcel_cloud.child_uu[passed_parent_parcel_idx], unit_radial_velocity);
-      CONVERGE_vec3_normalize(unit_radial_velocity); //unit vector in the radial direction of the parent parcel
+      CONVERGE_precision_t mag_check = CONVERGE_vec3_length(unit_radial_velocity)
+   
       
       CONVERGE_vec3_scale(unit_radial_velocity, parcel_cloud.radius[passed_parent_parcel_idx]); //need to displace the parcel by the parent's radius in the radial direction of the child parcel
       CONVERGE_vec3_add(parent_position, unit_radial_velocity,parcel_cloud.xx[passed_child_parcel_idx]);
@@ -213,6 +214,7 @@ CONVERGE_UDF(parcel_child,
             "parent_radius = %e\n"
              "parent_position = %e %e %e\n"
              "unit_radial_velocity = %e %e %e\n"
+             "unit_radial_velocity mag = %e\n"
              "child position = %e %e %e\n",
              parcel_cloud.radius[passed_parent_parcel_idx],
              parent_position[0], parent_position[1], parent_position[2],
