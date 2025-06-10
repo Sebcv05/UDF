ba864f3ef2d2ff227b0f570c63696d7de34d9b5a
leoqq <leofangxh@gmail.com>
2025-06-10 18:26:23 +0100

    Update

--- src/Breakup.c
+++ src/Breakup.c
@@ -217,7 +217,8 @@ if(old_parcel_cloud->thermal_breakup_flag[p_idx]==4){
                 //Calcualte velocity of each child parcel
 
                 CONVERGE_vec3_dup(child_velocity[nnn], new_parcel_uu); // Copy child's velocity from child_velocity array
-                CONVERGE_vec3_dup(child_velocity[nnn], old_parcel_cloud->child_uu[p_idx]);
+                printf("\nchild_uu[p_idx] = %e %e %e\n",old_parcel_cloud->child_uu[p_idx][0],old_parcel_cloud->child_uu[p_idx][1],old_parcel_cloud->child_uu[p_idx][2]);
+                // CONVERGE_vec3_dup(child_velocity[nnn], old_parcel_cloud->child_uu[p_idx]);
                 CONVERGE_vec3_add(new_parcel_uu, old_parcel_cloud->uu[p_idx], &new_parcel_uu);
                 // CONVERGE_vec3_dup(new_parcel_uu, old_parcel_cloud->child_uu[p_idx][nnn]); // Store child's velocity in old_parcel_cloud
 
