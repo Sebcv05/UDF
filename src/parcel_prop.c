a4b0d2c0ff3ed437d22c228bae3abe85c0e7e184
leoqq <leofangxh@gmail.com>
2025-06-24 16:27:57 +0100

    Update

--- src/parcel_prop.c
+++ src/parcel_prop.c
@@ -128,7 +128,7 @@ CONVERGE_UDF(parcel_child,
 
 
    // At the start of the function, with other variable declarations
-CONVERGE_precision_t *rel_vel = user_relative_velocity[(int)parcel_cloud.tbt[passed_parent_parcel_idx]];
+CONVERGE_precision_t *rel_vel = user_child_velocity[(int)parcel_cloud.tbt[passed_parent_parcel_idx]];
 
    // parcel_semi_mass_old = parcel_cloud.density[passed_child_parcel_idx] * parcel_cloud.radius[passed_child_parcel_idx] *
    //                        parcel_cloud.radius[passed_child_parcel_idx] * parcel_cloud.radius[passed_child_parcel_idx];
