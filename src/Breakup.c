6abe51d1d8b8a59c2762c51615bbdbc27a285926
leoqq <leofangxh@gmail.com>
2025-07-02 17:39:30 +0100

    update

--- src/Breakup.c
+++ src/Breakup.c
@@ -474,16 +474,21 @@ CONVERGE_precision_t calculated_radius = 1.0 / (2.0 * rad_denom * rad_term1 + ra
     
     // Update profiling information
     breakup_call_count++;
-    total_breakup_time += CONVERGE_mpi_wtime() - start_time;
+    total_breakup_time = CONVERGE_mpi_wtime() - start_time;
     
-    // Print profiling information periodically
-    printf("\nbreakup.c timing\n\n");
-    printf("\ninit_time = %e\n",init_time);
-    printf("\nbreakup_calc_time = %e\n",breakup_calc_time);
-    printf("\nchild_parcel_time = %e\n",child_parcel_time);
-    printf("\nzero_time = %e\n",zero_time);
-    printf("\ntotal_breakup_time = %e\n",total_breakup_time);
+//Timing Fractions
+CONVERGE_precision_t init_time_frac,breakup_calc_time_frac,child_parcel_time_frac,zero_time_frac;
+init_time_frac = init_time / total_breakup_time;
+breakup_calc_time_frac = breakup_calc_time / total_breakup_time;
+child_parcel_time_frac = child_parcel_time / total_breakup_time;
+zero_time_frac = zero_time / total_breakup_time;
 
+    // Print profiling information periodically
+    printf("\nbreakup.c total time = %e ms\n\n",total_breakup_time*1000);
+    printf("\ninit_time_frac = %e\n",init_time_frac);
+    printf("\nbreakup_calc_time_frac = %e\n",breakup_calc_time_frac);
+    printf("\nchild_parcel_time_frac = %e\n",child_parcel_time_frac);
+    printf("\nzero_time_frac = %e\n",zero_time_frac);
     
     old_parcel_cloud->tbreak_kh[p_idx] = old_parcel_cloud->thermal_breakup_flag[p_idx];
 }
