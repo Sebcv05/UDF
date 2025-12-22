5fa69b46222c5a36b15494af45f2f8d55d502cb8
leoqq <leofangxh@gmail.com>
2025-12-22 10:42:56 +0000

    update

--- src/parcel_prop.c
+++ src/parcel_prop.c
@@ -348,13 +348,13 @@ CONVERGE_UDF(parcel_child,
       int rank;
       CONVERGE_mpi_comm_rank(&rank);
 
-      parcel_cloud.uu[passed_child_parcel_idx][0] = user_child_velocity_x*0.75;
-      parcel_cloud.uu[passed_child_parcel_idx][1] = user_child_velocity_y*0.75;
-      parcel_cloud.uu[passed_child_parcel_idx][2] = user_child_velocity_z*0.75;
+      parcel_cloud.uu[passed_child_parcel_idx][0] = user_child_velocity_x*0.65;
+      parcel_cloud.uu[passed_child_parcel_idx][1] = user_child_velocity_y*0.65;
+      parcel_cloud.uu[passed_child_parcel_idx][2] = user_child_velocity_z*0.65;
       
-      parcel_cloud.uu_tm1[passed_child_parcel_idx][0] = user_child_velocity_x*0.75;
-      parcel_cloud.uu_tm1[passed_child_parcel_idx][1] = user_child_velocity_y*0.75;
-      parcel_cloud.uu_tm1[passed_child_parcel_idx][2] = user_child_velocity_z*0.75;
+      parcel_cloud.uu_tm1[passed_child_parcel_idx][0] = user_child_velocity_x*0.65;
+      parcel_cloud.uu_tm1[passed_child_parcel_idx][1] = user_child_velocity_y*0.65;
+      parcel_cloud.uu_tm1[passed_child_parcel_idx][2] = user_child_velocity_z*0.65;
 
     
       // Update positions based on normalized velocity direction
