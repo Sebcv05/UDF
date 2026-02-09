be53c28225be68a1c9903afc67059309b8f64454
leoqq <leofangxh@gmail.com>
2026-02-09 18:14:53 +0000

    update

--- src/spray_evap.c
+++ src/spray_evap.c
@@ -686,7 +686,7 @@ void spray_evap_cell(CONVERGE_cloud_t cloud)
 
                // ******************************************************************************************************//
          // Print  Droplet Data to File 5% sample rate
-         CONVERGE_precision_t user_rand = CONVERGE_random_precision();
+         // CONVERGE_precision_t user_rand = CONVERGE_random_precision();,
          if(user_rand<0.05){
          CONVERGE_precision_t vmag =  CONVERGE_sqrt( CONVERGE_square( parcel_cloud.uu[0][0]) + CONVERGE_square( parcel_cloud.uu[0][1]) + CONVERGE_square( parcel_cloud.uu[0][2]));
          CONVERGE_precision_t sim_time = CONVERGE_simulation_time_sec();
