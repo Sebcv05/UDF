d9e5a929ab9a0ec2c499a9af21398073e89ecbac
leoqq <leofangxh@gmail.com>
2026-02-09 18:16:07 +0000

    update

--- src/spray_evap.c
+++ src/spray_evap.c
@@ -650,9 +650,9 @@ void spray_evap_cell(CONVERGE_cloud_t cloud)
              CONVERGE_precision_t user_rand = CONVERGE_random_precision();
              if(user_rand < 0.05)
              {
-                 CONVERGE_precision_t vmag = CONVERGE_sqrt(CONVERGE_square(parcel_cloud.uu[i_pc][0]) + 
-                                                           CONVERGE_square(parcel_cloud.uu[i_pc][1]) + 
-                                                           CONVERGE_square(parcel_cloud.uu[i_pc][2]));
+                 CONVERGE_precision_t vmag = CONVERGE_sqrt(CONVERGE_square(parcel_cloud.uu[0][0]) + 
+                                                           CONVERGE_square(parcel_cloud.uu[0][1]) + 
+                                                           CONVERGE_square(parcel_cloud.uu[0][2]));
                  
                  CONVERGE_precision_t sim_time = CONVERGE_simulation_time_sec();
                  
@@ -662,15 +662,15 @@ void spray_evap_cell(CONVERGE_cloud_t cloud)
                  {
                      // Format: cloud_idx parcel_idx temp radius lifetime vmag is_child sim_time inj_time
                      fprintf(fp1, "%ld %ld %.6e %.6e %.6e %.6e %d %.6e %.6e\n", 
-                             parcel_cloud.cloud_index[i_pc], 
-                             parcel_cloud.parcel_index[i_pc], 
-                             parcel_cloud.temp[i_pc], 
-                             parcel_cloud.radius[i_pc], 
-                             parcel_cloud.lifetime[i_pc], 
+                             parcel_cloud.cloud_index[0], 
+                             parcel_cloud.parcel_index[0], 
+                             parcel_cloud.temp[0], 
+                             parcel_cloud.radius[0], 
+                             parcel_cloud.lifetime[0], 
                              vmag, 
-                             (parcel_cloud.breakup_phase[i_pc] >= 5) ? 1 : 0, 
+                             (parcel_cloud.breakup_phase[0] >= 5) ? 1 : 0, 
                              sim_time, 
-                             parcel_cloud.time_of_injection[i_pc]);
+                             parcel_cloud.time_of_injection[0]);
                      fclose(fp1);
                  }
                  else
