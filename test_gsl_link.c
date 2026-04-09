b881c2f92e148970872df2cb3fb99633cae621c8
leoqq <leofangxh@gmail.com>
2026-04-09 14:51:18 +0100

    update

--- test_gsl_link.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <gsl/gsl_math.h>
-
-int main() {
-    double x = gsl_pow_2(3.0);
-    printf("Result: %f\n", x);
-    return 0;
-}
--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git add . 
-git commit -m "Update"
-git push -u origin v3.1.12
