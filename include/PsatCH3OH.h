0af577f54b2da228aaa85ee0b9ac18c64122c6dd
Sebcv05 <sebastiancoxonv@gmail.com>
2026-07-23 12:28:06 +0100

    created new files and changes headers and comments to call new CH3OH.c files

---
+++ include/PsatCH3OH.h
@@ -0,0 +1,17 @@
+// PsatCH3OH.h
+
+#include "lagrangian/env.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <complex.h>
+
+
+#ifndef P_SAT_CH3OH_H
+#define P_SAT_CH3OH_H
+
+void Saturation_PressureCH3OH(CONVERGE_precision_t Td,CONVERGE_precision_t* P_sat);
+
+#endif // P_SAT_CH3OH_H
---
+++ include/TsatCH3OH.h
@@ -0,0 +1,20 @@
+//TsatCH3OH.h
+
+
+#include "lagrangian/env.h"
+#include <user_header.h>
+#include <CONVERGE/udf.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <complex.h>
+
+
+#ifndef T_SAT_CH3OH_H
+#define T_SAT_CH3OH_H
+
+CONVERGE_precision_t T_satCH3OH(CONVERGE_precision_t P);
+
+#endif // T_SAT_CH3OH_H
